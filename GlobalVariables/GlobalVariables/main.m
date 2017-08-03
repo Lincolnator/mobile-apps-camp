969e4d2e5929db9d12fcebaf1ad182d11a5b9ad1
Lincoln <Lincoln.d.dahlke@gmail.com>
2017-08-03 13:29:27 -0400

    My first codea project

---
+++ GlobalVariables/GlobalVariables/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Global Variables
+//
+//  Created by Lincoln on Thursday, August 3, 2017
+//  Copyright (c) Lincoln. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
